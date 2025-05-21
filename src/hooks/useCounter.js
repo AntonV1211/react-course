5cd34df17493e6a3d4dc468cd440e466fa586a0d
AntonV1211 <antonV1211@yandex.ru>
2025-05-21 22:10:28 +0700

    Fix. UseCounter. Edit the names of variables

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -3,8 +3,12 @@ import { useState } from 'react';
 export function useCounter({ min = 0, max = 5, initial = 0 } = {}) {
     const [count, setCount] = useState(initial);
 
-    const increment = () => setCount((c) => (c < max ? c + 1 : c));
-    const decrement = () => setCount((c) => (c > min ? c - 1 : c));
+    const increment = () => setCount(
+        (currentCount) => (currentCount < max ? currentCount + 1 : currentCount)
+    );
+    const decrement = () => setCount(
+        (currentCount) => (currentCount > min ? currentCount - 1 : currentCount)
+    );
 
     return { count, increment, decrement };
 }
