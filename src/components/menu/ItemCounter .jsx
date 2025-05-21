cd8f2eea9a8431ae826b280627a5647c50d30784
AntonV1211 <antonV1211@yandex.ru>
2025-05-21 22:14:27 +0700

    Fix. ItemCounter. Edit component name

--- src/components/menu/ItemCounter .jsx
+++ src/components/menu/ItemCounter .jsx
@@ -1,4 +1,4 @@
-export const Counter = ({ count, increment, decrement, min = 0, max = 5 }) => (
+export const ItemCounter = ({ count, increment, decrement, min = 0, max = 5 }) => (
     <div className='menu-item_counter'>
         <button onClick={decrement} disabled={count === min} style={{ marginLeft: 8 }}>-</button>
         <span style={{ margin: '0 8px' }}>{count}</span>
